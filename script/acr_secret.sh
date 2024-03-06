29b452cafa9ccbb35ab595ac7128f4b68a091861
Administrator <admin@example.com>
2024-03-06 07:43:57 +0000

    Update 2 files
    - /.gitlab-ci.yml
    - /script/acr_secret.sh

--- script/acr_secret.sh
+++ script/acr_secret.sh
@@ -1,4 +1,18 @@
+#!/bin/bash
+
+# Define Docker registry credentials
+DOCKER_SERVER="eshopimages.azurecr.io"
+DOCKER_USERNAME="eshopimages"
+DOCKER_PASSWORD="jvOvtdM20HNanYNgRO4G3TP7hACQX3454/GaQ0icWR+ACRAMue0q"
+
+# Check if the secret already exists
+if kubectl get secret acr-secret >/dev/null 2>&1; then
+    echo "Secret acr-secret already exists. Skipping creation."
+    exit 0
+fi
+
+# Create Docker registry secret
 kubectl create secret docker-registry acr-secret \
-    --docker-server=eshopimages.azurecr.io \
-    --docker-username=eshopimages \
-    --docker-password=jvOvtdM20HNanYNgRO4G3TP7hACQX3454/GaQ0icWR+ACRAMue0q
+    --docker-server="$DOCKER_SERVER" \
+    --docker-username="$DOCKER_USERNAME" \
+    --docker-password="$DOCKER_PASSWORD"
